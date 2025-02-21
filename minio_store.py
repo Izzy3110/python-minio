e5d20c307b3bcf69df395d24905a74574864c8ef
Sascha Frank <sfrank@wyl-online.de>
2025-02-21 06:44:01 +0100

    updated parameter _generate_pdf=True

--- minio_store.py
+++ minio_store.py
@@ -4,4 +4,4 @@ minio_client = MinioClient()
 
 
 if __name__ == '__main__':
-    minio_client.do_pdf_upload(pdf_filepath="sample.pdf", object_name="sample.pdf")
+    minio_client.do_pdf_upload(pdf_filepath="sample.pdf", object_name="sample.pdf", generate_pdf_=True)
